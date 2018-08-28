ce0d9ea07e5c92d46fe131bafb0c31034b02d92d
travispbuckley <travispbuckley@lewisu.edu>
2018-08-27 22:09:03 -0500

    add middleware for rending static directory in filesystem

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var app = express();
 // .get function takes in url path, and a function containing the request
 // and the response. Request: headers, body, method to path, etc. 
 // Response: the data being sent back: content, http status codes, etc.
+
+// Middleware allows you to use express for different desired functionality.
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express.</h1>');
     res.send({
@@ -24,5 +28,8 @@ app.get('/bad', (req, res) => {
     });
 });
 
-// binds application to port on machine.
-app.listen(3000);
+// binds application to port on machine. Can take a second parameter
+// a function to do something once server is up.
+app.listen(3000, () => {
+    console.log('server is up on port 3000');
+});
