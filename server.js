c5d45db35e5ae7cdd03100cfb15712b8e7bc58ba
travispbuckley <travispbuckley@lewisu.edu>
2018-09-04 22:00:10 -0500

    remove maintence page

--- server.js
+++ server.js
@@ -28,10 +28,10 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+//     next();
+// });
 
 app.use(express.static(__dirname + '/public'));
 
