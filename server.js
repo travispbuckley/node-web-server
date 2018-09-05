40981be536bfafecd4ca8b0a15c75abe2b19a666
travispbuckley <travispbuckley@lewisu.edu>
2018-09-04 22:10:12 -0500

    add projects route and view

--- server.js
+++ server.js
@@ -59,6 +59,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Project Page'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Error handling request.'
